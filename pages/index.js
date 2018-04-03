f8224a08c478ea71d40d7cf61521f0d6a00aeca9
Jimmy Moon <ragingwind@gmail.com>
2018-04-04 00:02:51 +0900

    add header and navigation

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,74 @@
 import Head from 'next/head'
+import Link from 'next/link'
+
+const Header = () => (
+  <header>
+    <nav>
+      <div className='left'>
+        <Link href='/'><a className='logo next-logo'></a></Link>
+      </div>
+      <div className='mid'>
+        <Link href='/'><a>Docs</a></Link>
+        <Link href='/'><a>About</a></Link>
+        <Link href='/'><a>Showcase</a></Link>
+        <Link href='/'><a>Github</a></Link>
+        <Link href='/'><a>Enterprise</a></Link>
+        <Link href='/'><a>Learn</a></Link>
+      </div>
+      <div className='right'>
+        <Link href='/'><a className='logo zeit-logo'></a></Link>
+      </div>
+    </nav>
+    <style jsx>{`
+      header {
+        display: flex;
+        width: 100%;
+        height: 70px;
+        justify-content: center;
+        align-items: center;
+        box-shadow: 0 2px 4px rgba(73, 60, 60, 0.12)
+      }
+      nav {
+        display: flex;
+        width: 1000px;
+        height: 50px;
+        justify-content: center;
+        align-items: center;
+      };
+      nav .left {
+        margin-right: 180px;
+      };
+      nav .mid {
+        flex-direction: row;
+      };
+      nav .mid a {
+        font-size: 14px;
+        color: #999999;
+        margin-right: 34px;
+        text-decoration: none;
+      };
+      nav .right {
+        margin-left: 180px;
+      };
+      .logo {
+        display: block;
+        position: relative;
+        background-size: contain;
+        background-repeat: no-repeat;
+      }
+      .next-logo {
+        width: 82px;
+        height: 50px;
+        background-image: url('/static/images/logotype-nextjs.png')
+      };
+      .zeit-logo {
+        width: 18px;
+        height: 16px;
+        background-image: url('/static/images/logotype-white.png')
+      };
+    `}</style>
+  </header>
+)
 
 export default () => (
   <div>
@@ -6,6 +76,7 @@ export default () => (
       <title>Next.js</title>
     </Head>
     <main>
+      <Header />
     </main>
     <footer>
     </footer>
