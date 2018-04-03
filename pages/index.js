4b0cd90d18e9f690771823f21d2eec86c40eeccf
Jimmy Moon <ragingwind@gmail.com>
2018-04-04 00:02:27 +0900

    structure initial document

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+
+export default () => (
+  <div>
+    <Head>
+      <title>Next.js</title>
+    </Head>
+    <main>
+    </main>
+    <footer>
+    </footer>
+    <style jsx global>{`
+      body {
+        padding: 0;
+        margin: 0;
+        min-height: 100%;
+        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
+        text-rendering: optimizeLegibility;
+      };
+    `}</style>
+  </div>
+)
