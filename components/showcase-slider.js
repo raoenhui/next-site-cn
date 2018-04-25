1560dd8ef810dc3061789825d8ac49d6c7b6e06c
Jimmy Moon <ragingwind@gmail.com>
2018-04-25 20:47:47 +0900

    use a brand new slider

--- components/showcase-slider.js
+++ components/showcase-slider.js
@@ -1,5 +1,7 @@
 import {Component} from 'react'
+import Link from 'next/link'
 import Icons from '../components/icons'
+import Manifest from '../components/showcase-manifest'
 
 const Navigation = ({children, prev, onClick}) => (
   <div className={`button ${prev ? 'prev' : 'next'}`} onClick={onClick}>
@@ -12,8 +14,8 @@ const Navigation = ({children, prev, onClick}) => (
     </div>
     <style jsx>{`
       .button {
-        width: 160px;
-        height: 180px;
+        width: 100%;
+        height: 100%;
         display: flex;
         align-items: center;
         justify-content: center;
@@ -46,9 +48,115 @@ const Navigation = ({children, prev, onClick}) => (
   </div>
 )
 
+class LazyImage extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      src: undefined,
+      image: props.image,
+      phClassName: 'placeholder',
+      srcClassName: 'src'
+    }
+  }
+
+  componentWillMount() {
+    if (typeof window !== 'undefined') {
+      this.load()
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.image.src !== this.state.image.src) {
+      this.setState({
+        image: nextProps.image,
+        phClassName: 'placeholderd',
+        srcClassName: 'src'
+      });
+      setTimeout(() => {
+        if (typeof window !== 'undefined') {
+          this.load()
+        }
+      }, 100);
+    }
+  }
+
+  load() {
+    const img = new Image()
+    img.onload = () => {
+      this.setState({
+        src: this.state.image.src,
+        phClassName: 'placeholder loaded',
+        srcClassName: 'src loaded'
+      })
+    }
+    img.src = this.state.image.src
+  }
+
+  render() {
+    return (
+      <div>
+        <img src={this.state.image.data} className={this.state.phClassName} />
+        <img src={this.state.src} className={this.state.srcClassName} />
+        <style jsx>{`
+          img {
+            width: 100%;
+            min-height: 0;
+            box-shadow: 0 30px 40px rgba(0, 0, 0, 0.12);
+          };
+          img.src {
+            opacity: 0;
+            position: absolute;
+            left: 0;
+          };
+          img.src.loaded {
+            opacity: 1;
+            transition: opacity 2s ease-in-out;
+          }
+          img.placeholder {
+            opacity: 1;
+            transition: opacity 2s ease-in-out;
+          };
+          img.placeholder.loaded {
+            opacity: 0;
+            transition: opacity 2s ease-in-out;
+          };
+        `}</style>
+      </div>
+    )
+  }
+}
+
 export default class Slider extends Component {
   constructor() {
     super()
+
+    this.next = this.next.bind(this)
+    this.prev = this.prev.bind(this)
+
+    this.state = {
+      current: 0,
+      currentImage: Manifest.manifest[0],
+      prevImage: Manifest.manifest[Manifest.prev(0)],
+      nextImage: Manifest.manifest[Manifest.next(0)]
+    }
+  }
+
+  change(current) {
+    this.setState({
+      current: current,
+      currentImage: Manifest.manifest[current],
+      prevImage: Manifest.manifest[Manifest.prev(current)],
+      nextImage: Manifest.manifest[Manifest.next(current)]
+    })
+  }
+
+  next() {
+    this.change(Manifest.next(this.state.current))
+  }
+
+  prev() {
+    this.change(Manifest.prev(this.state.current))
   }
 
   render() {
@@ -56,56 +164,86 @@ export default class Slider extends Component {
       <div className='container'>
         <div className='slides'>
           <div className='slide prev'>
-            <div className='viwer'>
-              <Navigation prev>
-                <img src='/static/images/showcases/magic-leap.png' />
-              </Navigation>
-            </div>
+            <Navigation prev onClick={this.prev}>
+              <LazyImage image={this.state.prevImage} />
+            </Navigation>
           </div>
           <div className='slide center'>
-            <div className='viwer'>
-              <img src='/static/images/showcases/magic-leap.png' />
+            <div className='image'>
+              <LazyImage ref={this.current} image={this.state.currentImage} />
             </div>
           </div>
           <div className='slide next'>
-            <div className='viwer'>
-              <Navigation>
-                <img src='/static/images/showcases/magic-leap.png' />
-              </Navigation>
-            </div>
+            <Navigation next onClick={this.next}>
+              <LazyImage image={this.state.nextImage} />
+            </Navigation>
           </div>
         </div>
         <style jsx>{`
           .container {
-            width: 100%;
-            height: auto;
-            min-height: 400px;
+            display: block;
+            margin: 0 auto;
+            padding: 20px 0;
           };
           .slides {
+            padding: 25px 0 25px 0;
+            width: 100%;
             display: flex;
-            justify-content: center;
             align-items: center;
+            justify-content: space-between;
+            will-change: opacity;
           };
-          .slide {
-            display: flex;
-          }
-          .slide.center {
-            margin: 0 25px;
-            flex: 2.2;
-            box-shadow: 0 30px 40px rgba(0, 0, 0, 0.12);
+          .image {
+            width: 1280px;
+            height: 734px;
+            display: block;
+            position: relative;
           };
           .slide.prev {
-            justify-content: flex-start;
-            align-items: center;
-            flex: 0.6;
+            width: 160px;
+            min-width: 160px;
+            height: 180px;
           };
           .slide.next {
-            justify-content: flex-end;
-            align-items: center;
-            flex: 0.6;
+            width: 160px;
+            min-width: 160px;
+            height: 180px;
           };
-          img {
-            width: 100%
+          @media (max-width: 1600px) {
+            .image {
+              width: 1024px;
+              height: 587px;
+            }
+          };
+          @media (max-width: 1360px) {
+            .image {
+              width: 720px;
+              height: 413px;
+            }
+          };
+          @media (max-width: 1060px) {
+            .image {
+              width: 470px;
+              height: 270px;
+            }
+          };
+          @media (max-width: 900px) {
+            .slide.prev {
+              width: 0;
+              min-width: 0;
+            };
+            .slide.next {
+              width: 0;
+              min-width: 0;
+            };
+            .slides {
+              width: 90%;
+              margin: 0 auto;
+            }
+            .image {
+              width: 100%;
+              height: auto;
+            }
           }
         `}</style>
       </div>
--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,24 @@
 import Document, { Head, Main, NextScript } from 'next/document'
 import flush from 'styled-jsx/server'
+import Link from 'next/link'
+import ShowcaseManifest from '../components/showcase-manifest'
 
 export default class MyDocument extends Document {
-  static async getInitialProps({renderPage}) {
-    return {
+  static async getInitialProps({pathname, renderPage}) {
+    const ctx =  {
       ...renderPage(),
       styles: flush()
     }
+
+    if (pathname === '/showcase') {
+      // prefetching three items of images.
+      // rest of images will be pending if they are listed
+      ctx.linksPrefetch = ShowcaseManifest.manifest
+        .slice(0, 3)
+        .map((s, i) => <link key={i} rel='prefetch' href={`${s.src}`} />)
+    }
+
+    return ctx
   }
 
   render() {
@@ -15,6 +27,7 @@ export default class MyDocument extends Document {
         <Head>
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
+          {this.props.linksPrefetch}
         </Head>
         <body>
           <Main />
