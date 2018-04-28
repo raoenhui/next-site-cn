5eec2aa5eee24944db5e9537a3d85f3bd3417d52
timothyis <hello@timothy.is>
2018-04-28 00:33:27 -0700

    Meta, twitter cards, etc.

--- components/docs/head.js
+++ components/docs/head.js
@@ -4,10 +4,6 @@ export default ({ children, ...props }) => (
   <Head>
     <title>{`${props.title} - Next.js Documentation`}</title>
     <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1.0"
-    />
-    <meta
       name="twitter:card"
       content={props.image ? 'summary_large_image' : 'summary'}
     />
@@ -15,10 +11,6 @@ export default ({ children, ...props }) => (
     <meta
       name="og:title"
       content={props.ogTitle || props.title}
-    />
-    <meta
-      name="og:url"
-      content={props.url || 'https://zeit.co/now'}
     />
     {props.description ? (
       <meta name="description" content={props.description} />
@@ -28,12 +20,6 @@ export default ({ children, ...props }) => (
     ) : (
       <meta name="og:description" content={props.description} />
     )}
-    <meta
-      name="og:image"
-      content={
-        props.image || `/static/images/twitter-card.png`
-      }
-    />
     {props.video
       ? [
         <meta name="og:type" content="video" key="0" />,
@@ -41,19 +27,6 @@ export default ({ children, ...props }) => (
         <meta name="og:video:type" content="video/mp4" key="2" />
       ]
       : null}
-
-    {/*
-      Favicons
-    */}
-    <link rel="apple-touch-icon" sizes="180x180" href="/static/favicon/apple-touch-icon.png" />
-    <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon/favicon-32x32.png" />
-    <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon/favicon-16x16.png" />
-    <link rel="manifest" href="/static/favicon/site.webmanifest" />
-    <link rel="mask-icon" href="/static/favicon/safari-pinned-tab.svg" color="#000000" />
-    <link rel="shortcut icon" href="/static/favicon/favicon.ico" />
-    <meta name="msapplication-TileColor" content="#000000" />
-    <meta name="msapplication-config" content="/static/favicon/browserconfig.xml" />
-    <meta name="theme-color" content="#000" />
 
     { children }
   </Head>
--- pages/_document.js
+++ pages/_document.js
@@ -23,10 +23,38 @@ export default class MyDocument extends Document {
 
   render() {
     return (
-      <html>
+      <html lang="en">
         <Head>
-          <meta name="theme-color" content="#000000" />
-          <meta name="viewport" content="width=device-width" initial-scale="1" />
+          {/*
+            Meta
+          */}
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          <meta name="description" content="Next.js is a lightweight framework for static and server-rendered applications" />
+          <meta property="twitter:card" content="https://nextjs.org/static/images/twitter_card.png" />
+          <meta property="twitter:site" content="@zeithq" />
+          <meta property="og:type" content="website" />
+          <meta propetty="og:url" content="https://nextjs.org" />
+          <meta property="og:title" content="Next.js" />
+          <meta property="og:description" content="Next.js is a lightweight framework for static and server-rendered applications" />
+          <meta property="og:image" content="https://nextjs.org/static/images/twitter_card.png" />
+
+
+          {/*
+            Favicons
+          */}
+          <link rel="apple-touch-icon" sizes="180x180" href="/static/favicon/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon/favicon-16x16.png" />
+          <link rel="manifest" href="/static/favicon/site.webmanifest" />
+          <link rel="mask-icon" href="/static/favicon/safari-pinned-tab.svg" color="#000000" />
+          <link rel="shortcut icon" href="/static/favicon/favicon.ico" />
+          <meta name="msapplication-TileColor" content="#000000" />
+          <meta name="msapplication-config" content="/static/favicon/browserconfig.xml" />
+          <meta name="theme-color" content="#000" />
+
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
           {this.props.linksPrefetch}
