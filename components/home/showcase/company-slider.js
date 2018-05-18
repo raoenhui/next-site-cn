3a34c5eaedbca80d35fa15b9ec7338dda46acf16
Tim Neutkens <timneutkens@me.com>
2018-05-18 13:11:53 +0200

    Make sure there is no outline or cursor where can’t be clicked

--- components/home/showcase/company-slider.js
+++ components/home/showcase/company-slider.js
@@ -33,16 +33,25 @@ const settings = {
 };
 
 export default () => (
-  <Slider {...settings}>
-    <div style={{width:388}}><Icons.OpenCollective /></div>
-    <div style={{width:140}}><Icons.Eaze /></div>
-    <div style={{width:176}}><Icons.MagicLeap /></div>
-    <div style={{width:180}}><Icons.Trulia /></div>
-    <div style={{width:238}}><Icons.MozillaVR /></div>
-    <div style={{width:172}}><Icons.Netflix /></div>
-    <div style={{width:172}}><Icons.GithubLogo /></div>
-    <div style={{width:155}}><Icons.Scale /></div>
-    <div style={{width:181}}><Icons.Auth0 /></div>
-    <div style={{width:285}}><Icons.Ticketmaster /></div>
-  </Slider>
+  <div className="company-slider">
+    <Slider {...settings}>
+      <div style={{width:388}}><Icons.OpenCollective /></div>
+      <div style={{width:140}}><Icons.Eaze /></div>
+      <div style={{width:176}}><Icons.MagicLeap /></div>
+      <div style={{width:180}}><Icons.Trulia /></div>
+      <div style={{width:238}}><Icons.MozillaVR /></div>
+      <div style={{width:172}}><Icons.Netflix /></div>
+      <div style={{width:172}}><Icons.GithubLogo /></div>
+      <div style={{width:155}}><Icons.Scale /></div>
+      <div style={{width:181}}><Icons.Auth0 /></div>
+      <div style={{width:285}}><Icons.Ticketmaster /></div>
+    </Slider>
+    <style jsx>{`
+      .company-slider div {
+        cursor: default;
+        outline: none;
+      }
+    `}</style>
+  </div>
+  
 )
