c068b1ecf811a5005fccf8ee51800a241dc332f5
Tim Neutkens <tim@timneutkens.nl>
2018-05-22 22:01:30 +0200

    Add screen to homepage (#13)

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,13 @@ import Page from '../components/page'
 import Intro from '../components/home/intro'
 import Showcase from '../components/home/showcase'
 import Features from '../components/home/features'
+import Screen from '../components/screen'
 
 function Index() {
   return <Page>
-    <Intro/>
+    <Screen offset={144}>
+      <Intro/>
+    </Screen>
     <Showcase />
     <Features />
   </Page>
