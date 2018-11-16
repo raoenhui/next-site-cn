0b77fc245a5056d98486e4c89dc5c75a130ada08
raoenhui <raoenhui@jd.com>
2018-11-16 16:37:06 +0800

    link bugfix

--- 7.0.0-canary.11/_next/static/commons/manifest.js
+++ 7.0.0-canary.11/_next/static/commons/manifest.js
@@ -90,7 +90,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "6698bbe8721aa63fe1ee"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "3f6432d08ddfd9822eeb"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
@@ -755,7 +755,7 @@
 /******/ 		if (__webpack_require__.nc) {
 /******/ 			script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 		}
-/******/ 		script.src = __webpack_require__.p + "" + ({"0":"main.js","1":"bundles/pages/_document.js","2":"bundles/pages/_error.js","3":"bundles/pages/_app.js","6":"bundles/pages/docs.js"}[chunkId]||chunkId) + ".js";
+/******/ 		script.src = __webpack_require__.p + "" + ({"0":"main.js","1":"bundles/pages/_document.js","2":"bundles/pages/_error.js","3":"bundles/pages/_app.js","5":"bundles/pages/docs.js"}[chunkId]||chunkId) + ".js";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {
--- README.md
+++ README.md
@@ -1,13 +1,18 @@
 # 前言
 Next.js 是一个轻量级的 React 服务端渲染应用框架。
 
-本站就是next.js编写的，是个很好的 Demo, ^ ^.
-
 next.js中文文档:[http://nextjs.frontendx.cn](http://nextjs.frontendx.cn)
 
 当前翻译版本为 7.0.0-canary.8。
 
-讨论组：[https://gitter.im/next-site-cn/nextjs?utm_source=share-link&utm_medium=link&utm_campaign=share-link](https://gitter.im/next-site-cn/nextjs?utm_source=share-link&utm_medium=link&utm_campaign=share-link)
+
+## 案例
+ - 用nextjs + express + MongoDB的增删改查项目:[程序员英语学习](https://github.com/raoenhui/code-english)
+    
+ - 本站就是next.js编写的生成静态网站，是个很好的 Demo, ^ ^.
+ 
+## 讨论组
+ [https://gitter.im/next-site-cn/nextjs?utm_source=share-link&utm_medium=link&utm_campaign=share-link](https://gitter.im/next-site-cn/nextjs?utm_source=share-link&utm_medium=link&utm_campaign=share-link)
 
 ## 安装运行
 
