1e581f2b222851927fdad64ce73ec8774366ac6f
raoenhui <raoenhui@jd.com>
2019-10-10 21:15:25 +0800

    fixup link about component

--- 7.0.0-canary.11/_next/static/commons/manifest.js
+++ 7.0.0-canary.11/_next/static/commons/manifest.js
@@ -90,7 +90,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "464c70bebd46eecbbbdf"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "1aeeb85edb3480681791"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
